f25ed88eaa2769fe82c49d77b27b2a819d023b16
meghaDhiomics <84699361+meghaDhiomics@users.noreply.github.com>
2022-11-02 18:24:07 +0530

    Updated Code

--- official-team-site/pages/maintemplate/get-In-touch/GetInTouch.js
+++ official-team-site/pages/maintemplate/get-In-touch/GetInTouch.js
@@ -6,7 +6,7 @@ export default function GetInTouch(){
     return(
         <Grid md={12} className={styles.GetInTouchBackground}>
         <Typography className={`${styles.GetInTouchHeading} ${"pt-5"}`}>Investing in India</Typography><br/>
-        <Typography className={""}>We look forward to hearing your requirement</Typography>
+        <Typography >We look forward to hearing your requirement</Typography>
         </Grid>
     )
 }
